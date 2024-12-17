4a549cd7dc26eda1604b5837573fa8c214b0f65e
John Xanthopoulos <johxan@MacBookPro.local>
2024-12-17 09:25:08 -0500

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import DrivewayCalculator from "./components/DrivewayCalculator";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <DrivewayCalculator />
+  </React.StrictMode>
+);
